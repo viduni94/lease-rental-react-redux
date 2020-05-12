02c93575525fd8589423eba0beac648514122d59
vwic5885 <vwic5885@sysco.com>
2020-05-12 11:50:25 +0530

    Added app test

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,11 @@ module.exports = {
     "!<rootDir>/node_modules/",
   ],
   setupFilesAfterEnv: ["<rootDir>/config/setupTest.js"],
-  "transform": {
+  transform: {
     "^.+\\.jsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js"
   }
 };
